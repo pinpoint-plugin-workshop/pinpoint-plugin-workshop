654581e90f2de9d392568da53547ed83ca134b88
Koo Taejin <koo.taejin@navercorp.com>
2019-10-31 14:34:44 +0900

    add pizzaName and add annotation

--- workshop-pizza-shop/src/main/java/com/pinpoint/workshop/pizza/shop/vo/CompletedPizzaOrder.java
+++ workshop-pizza-shop/src/main/java/com/pinpoint/workshop/pizza/shop/vo/CompletedPizzaOrder.java
@@ -7,12 +7,14 @@ public class CompletedPizzaOrder {
 
     private final String orderId;
     private final String customerName;
+    private final String pizzaName;
     private final int pizzaQuantity;
     private final boolean orderSuccessful;
 
-    public CompletedPizzaOrder(String orderId, String customerName, int pizzaQuantity, boolean orderSuccessful) {
+    public CompletedPizzaOrder(String orderId, String customerName, String pizzaName, int pizzaQuantity, boolean orderSuccessful) {
         this.orderId = orderId;
         this.customerName = customerName;
+        this.pizzaName = pizzaName;
         this.pizzaQuantity = pizzaQuantity;
         this.orderSuccessful = orderSuccessful;
     }
@@ -23,6 +25,10 @@ public class CompletedPizzaOrder {
 
     public String getCustomerName() {
         return customerName;
+    }
+
+    public String getPizzaName() {
+        return pizzaName;
     }
 
     public int getPizzaQuantity() {
